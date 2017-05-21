daf4f820a659b21b7eb58583cffd1139b8a7602f
guillermot <guillet84@gmail.com>
2017-05-21 14:13:56 -0300

    guide list, place list, panto

--- src/assets/js/places.js
+++ src/assets/js/places.js
@@ -1,12 +1,13 @@
-const guides = [{
+const guidesList = [{
         name: 'First visit!',
         description: 'All places you need to go in your first visit to New York.',
+        img: '/src/assets/img/manhattan/rockefeller-center.jpg',
         places: [{
                 id: 1,
                 type: 'place',
                 name: 'Rockerfeller Center',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/rockefeller-center.jpg',
+                img: '/src/assets/img/manhattan/rockefeller-center.jpg',
                 lat: 40.758891,
                 lng: -73.978693
             },
@@ -15,14 +16,14 @@ const guides = [{
                 type: 'area',
                 name: 'Chinatown',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/chinatown.jpg'
+                img: '/src/assets/img/manhattan/chinatown.jpg'
             },
             {
                 id: 4,
                 type: 'place',
                 name: 'Grand Central Terminal',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/gran-central-terminal.jpg',
+                img: '/src/assets/img/manhattan/gran-central-terminal.jpg',
                 lat: 40.752954,
                 lng: -73.977208
             },
@@ -31,7 +32,7 @@ const guides = [{
                 type: 'place',
                 name: 'Brooklyn Bridge',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/brookyn-bridge.jpg',
+                img: '/src/assets/img/manhattan/brookyn-bridge.jpg',
                 lat: 40.706574,
                 lng: -73.996810
             },
@@ -40,7 +41,7 @@ const guides = [{
                 type: 'place',
                 name: 'Statue of Liberty',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/statue-of-liberty.jpg',
+                img: '/src/assets/img/manhattan/statue-of-liberty.jpg',
                 lat: 40.689225,
                 lng: -74.044533
             },
@@ -49,19 +50,20 @@ const guides = [{
                 type: 'area',
                 name: 'High Line',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/high-line.jpg'
+                img: '/src/assets/img/manhattan/high-line.jpg'
             }
         ]
     },
     {
         name: 'Central Park',
         description: 'This park is full of litles treasures to discover, each one of them will blow your mind.',
+        img: '/src/assets/img/manhattan/high-line.jpg',
         places: [{
                 id: 8,
                 type: 'place',
                 name: 'Belvedere Castle',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/high-line.jpg',
+                img: '/src/assets/img/manhattan/high-line.jpg',
                 lat: 40.779467,
                 lng: -73.968817
             },
@@ -70,7 +72,7 @@ const guides = [{
                 type: 'place',
                 title: 'Strawberry Fields Memorial',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/high-line.jpg',
+                img: '/src/assets/img/manhattan/high-line.jpg',
                 lat: 40.775738,
                 lng: -73.975186
             },
@@ -79,7 +81,7 @@ const guides = [{
                 type: 'place',
                 name: 'Bethesda Terrace and Fountain',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/high-line.jpg',
+                img: '/src/assets/img/manhattan/high-line.jpg',
                 lat: 40.773779,
                 lng: -73.970518
             },
@@ -88,7 +90,7 @@ const guides = [{
                 type: 'place',
                 name: 'Gapstow Bridge',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/high-line.jpg',
+                img: '/src/assets/img/manhattan/high-line.jpg',
                 lat: 40.766911,
                 lng: -73.973668
             },
@@ -97,7 +99,7 @@ const guides = [{
                 type: 'place',
                 name: 'Central Park Zoo',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/high-line.jpg',
+                img: '/src/assets/img/manhattan/high-line.jpg',
                 lat: 40.768004,
                 lng: -73.971848
             }
@@ -106,12 +108,13 @@ const guides = [{
     {
         name: 'Downtown',
         description: 'One of the most important financial',
+        img: '/src/assets/img/manhattan/high-line.jpg',
         places: [{
                 id: 13,
                 type: 'place',
                 name: 'World Trade Center',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/high-line.jpg',
+                img: '/src/assets/img/manhattan/high-line.jpg',
                 lat: 40.711802,
                 lng: -73.013118
             },
@@ -120,7 +123,7 @@ const guides = [{
                 type: 'place',
                 name: 'Charging Bull',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/high-line.jpg',
+                img: '/src/assets/img/manhattan/high-line.jpg',
                 lat: 40.705552,
                 lng: -73.013446
             },
@@ -130,7 +133,7 @@ const guides = [{
                 type: 'place',
                 name: 'Brooklyn Bridge',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/brookyn-bridge.jpg',
+                img: '/src/assets/img/manhattan/brookyn-bridge.jpg',
                 lat: 40.706574,
                 lng: -73.996810
             }
@@ -139,12 +142,13 @@ const guides = [{
     {
         name: 'Neighborhoods',
         description: 'Most important neighborhoods of Manhattan. All the right places you should visit',
+        img: '/src/assets/img/manhattan/high-line.jpg',
         places: [{
                 id: 15,
                 type: 'place',
                 name: 'Upper West Side',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/high-line.jpg',
+                img: '/src/assets/img/manhattan/high-line.jpg',
                 lat: 40.787159,
                 lng: -73.975835
             },
@@ -153,7 +157,7 @@ const guides = [{
                 type: 'place',
                 name: 'Greenwich Village',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/high-line.jpg',
+                img: '/src/assets/img/manhattan/high-line.jpg',
                 lat: 40.733323,
                 lng: -73.003037
             },
@@ -162,7 +166,7 @@ const guides = [{
                 type: 'place',
                 name: 'Upper East Side',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/high-line.jpg',
+                img: '/src/assets/img/manhattan/high-line.jpg',
                 lat: 40.773680,
                 lng: -73.957356
             },
@@ -171,7 +175,7 @@ const guides = [{
                 type: 'place',
                 name: 'Midtown',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/high-line.jpg',
+                img: '/src/assets/img/manhattan/high-line.jpg',
                 lat: 40.754514,
                 lng: -73.985984
             },
@@ -180,7 +184,7 @@ const guides = [{
                 type: 'place',
                 name: 'Downtown',
                 description: 'Iconic Midtown business complex',
-                img: 'assets/img/manhattan/high-line.jpg',
+                img: '/src/assets/img/manhattan/high-line.jpg',
                 lat: 40.722913,
                 lng: -74.000018
             },
